ffce12fecd94d7cf620724242124a5f66270773d
Pratik Dahal <dahalpratik50@gmail.com>
2025-04-24 19:00:20 +0545

    tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "##151312",
+        accent: "#AB8BFF",
+        light: {
+          100: "#D6C6FF",
+          200: "#A8B5DB",
+          300: "#9CA4AB",
+        },
+        dark: {
+          100: "#221F3D",
+          200: "#0F0D23",
+        },
+        background: "#F0F8FF",
+        text: "#000000",
+      },
+    },
   },
   plugins: [],
 };
